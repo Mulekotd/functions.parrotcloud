6b9159c17293768d2bb3d3c326119b2871982761
joao.gama-at-557777286813 <joaopedro.gama0675@gmail.com>
2025-01-11 23:10:39 -0300

    feat: implementing user cart subcollection

--- functions/src/utils/index.ts
+++ functions/src/utils/index.ts
@@ -1,6 +1,10 @@
+import crypto from "crypto";
+
 export const generateDisplayNameFromEmail = (email?: string): string | null => {
   if (!email) return null;
 
   const [name] = email.split("@");
   return name;
 };
+
+export const generateUUID = () => crypto.randomUUID();
